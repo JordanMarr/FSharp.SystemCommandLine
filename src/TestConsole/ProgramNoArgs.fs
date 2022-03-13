a0ed7c103d5e6d21e4ef085ec15c630cc1b573d6
Jordan Marr <jmarr@microdesk.com>
2022-03-13 17:11:13 -0400

    Removed unused sample code

--- src/TestConsole/ProgramNoArgs.fs
+++ src/TestConsole/ProgramNoArgs.fs
@@ -1,6 +1,5 @@
 ﻿module ProgramNoArgs
 
-open System.IO
 open FSharp.SystemCommandLine
     
 let app () = 
@@ -8,10 +7,6 @@ let app () =
 
 //[<EntryPoint>]
 let main argv = 
-    let intOption = Input.Option("--int-option", getDefaultValue = (fun () -> 42), description = "An option whose argument is parsed as an int")
-    let boolOption = Input.Option<bool>("--bool-option", "An option whose argument is parsed as a bool")
-    let fileOption = Input.Option<FileInfo>("--file-option", "An option whose argument is parsed as a FileInfo")
-
     rootCommand {
         description "My sample app"
         setHandler app
