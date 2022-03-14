9487b293add52a7a293064a59d14c3548895300f
Jordan Marr <jmarr@microdesk.com>
2022-03-14 19:59:45 -0400

    refactor test

--- src/Tests/SimpleAppTest.fs
+++ src/Tests/SimpleAppTest.fs
@@ -5,23 +5,20 @@ open FSharp.SystemCommandLine
 open Utils
 open FsUnit
 
-let cmd (argString: string) (handler: string[] * string -> unit) =
+[<Test>]
+let Test1 () =    
     let words = Input.Option(["--word"; "-w"], (fun () -> Array.empty<string>), "A list of words to be appended")
     let separator = Input.Option(["--separator"; "-s"], (fun () -> ","), "A character that will separate the joined words.")
-    
-    testRootCommand argString {
+
+    testRootCommand "--word Hello -w World -s *" {
         description "Appends words together"
         inputs (words, separator)
-        setHandler handler
+        setHandler (
+            fun (words: string array, separator: string) ->
+                words |> should equal [| "Hello"; "World" |]
+                separator |> should equal "*"
+                System.String.Join(separator, words) |> printfn "Result: %s"
+        )
     } 
     |> ignore
-
-[<Test>]
-let Test1 () =
-    let handler (words: string array, separator: string) =
-        words |> should equal [| "Hello"; "World" |]
-        separator |> should equal "*"
-        System.String.Join(separator, words) |> printfn "Result: %s"
-
-    cmd "--word Hello -w World -s *" handler
         
