8039ac4c3537fe35354780fea6ebd966aace20a1
shayanhabibi <shayan.habibi01@gmail.com>
2026-09-04 10:05:37 +0800

    chore(docs): new utilities do not explicitly set arity

--- src/FSharp.SystemCommandLine/Inputs.fs
+++ src/FSharp.SystemCommandLine/Inputs.fs
@@ -418,6 +418,7 @@ module Input =
         acceptOnlyFromChoicesWith StringComparer.Ordinal choices input
     
     /// Maps an option whose legal values are a known set bound to a typed value. Case sensitive.
+    /// Does not modify arity.
     let acceptManyFromChoices (choices: seq<string * 'T>) (input: ActionInput<'T list>) =
         acceptManyFromChoicesWith StringComparer.Ordinal choices input
         
@@ -426,6 +427,7 @@ module Input =
         acceptOnlyFromChoicesWith StringComparer.OrdinalIgnoreCase choices input
         
     /// Maps an option whose legal values are a known set bound to a typed value. Case insensitive.
+    /// Does not modify arity.
     let acceptManyFromChoicesIgnoreCase (choices: seq<string * 'T>) (input: ActionInput<'T list>) =
         acceptManyFromChoicesWith StringComparer.OrdinalIgnoreCase choices input
         
